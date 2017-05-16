cba70c25c7d467a43c4e1bf8e762d0c6ada9e0a4
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-16 21:20:11 +0200

    CarReg now start from separate class

---
+++ src/StartCarReg.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Alex on 2017-05-16.
+ */
+public class StartCarReg {
+    public static void main(String[] args) throws Exception {
+        FrontEnd CarReg = new FrontEnd();
+        CarReg.run();
+    }
+}
